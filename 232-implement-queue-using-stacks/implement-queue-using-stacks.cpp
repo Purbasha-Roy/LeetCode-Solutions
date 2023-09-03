94bf069dcfd8ea4acb1201ffeec5455364cef00b
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2023-09-03 15:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (39.39%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,36 +1,56 @@
 class MyQueue {
 public:
-stack<int> s1;
-stack<int> s2;
+// stack<int> s1;
+// stack<int> s2;
+stack<int> in;
+stack<int> out;
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s1.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
+        // while(!s1.empty()){
+        //     s2.push(s1.top());
+        //     s1.pop();
+        // }
+        // s1.push(x);
+        // while(!s2.empty()){
+        //     s1.push(s2.top());
+        //     s2.pop();
+        // }
+        in.push(x);
     }
     
     int pop() {
-        int x=s1.top();
-        s1.pop();
-        return x;
+        // int x=s1.top();
+        // s1.pop();
+        // return x;
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        int ans=out.top();
+        out.pop();
+        return ans;
     }
     
     int peek() {
-        int x=s1.top();
-        return x;
+        // int x=s1.top();
+        // return x;
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
     }
     
     bool empty() {
-        return s1.empty();
+        // return s1.empty();
+        return in.empty() && out.empty();
     }
 };
 
