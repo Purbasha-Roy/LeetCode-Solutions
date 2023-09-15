0b2df87e707dcd9b8c2591d22b26e96cb6524787
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2023-09-15 13:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (5.82%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,29 +11,46 @@
  */
 class Solution {
 public:
+void rightview(TreeNode* root, int level, vector<int>& ans){
+    if(root==NULL){
+        return;
+    }
+    if(ans.size()==level){
+        ans.push_back(root->val);
+    }
+    rightview(root->right,level+1,ans);
+    rightview(root->left,level+1,ans);
+}
     vector<int> rightSideView(TreeNode* root) {
+        // METHDO 1
+        // vector<int> v;
+        // if(root==NULL){
+        //     return v;
+        // }
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         if(i==size-1){
+        //             v.push_back(node->val);
+        //         }
+        //         if(node->left){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right){
+        //             q.push(node->right);
+        //         }
+        //     }
+        // }
+        // return v;
+
+        // METHOD 2
         vector<int> v;
-        if(root==NULL){
-            return v;
-        }
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(i==size-1){
-                    v.push_back(node->val);
-                }
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-        }
+        rightview(root,0,v);
         return v;
+
     }
 };
