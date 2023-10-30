789c92bcd37c3b32f8c8db52c9bf20377d724d86
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2023-10-30 13:25:18 +0530

    Time: 12 ms (48.84%) | Memory: 15.3 MB (64.14%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -12,28 +12,28 @@
 class Solution {
 public:
 int value=0;
-    int helperfunction(TreeNode* root){
-        if(root==NULL){
-            return value;
-        }
-        if(root->val==1){
-            value=1;
-        }
-        if(root->val==0){
-            value=0;
-        }
-        int left=helperfunction(root->left);
-        int right=helperfunction(root->right);
-        if(root->val==2){
-            value=left|right;
-        }
-        if(root->val==3){
-            value=left&right;
-        }
+int solve(TreeNode* root){
+    if(root==NULL){
         return value;
     }
+    if(root->val==1){
+        value=1;
+    }
+    else if(root->val==0){
+        value=0;
+    }
+    int left=solve(root->left);
+    int right=solve(root->right);
+    if(root->val==2){
+        value=left|right;
+    }
+    else if(root->val==3){
+        value=left&right;
+    }
+    return value;
+}
     bool evaluateTree(TreeNode* root) {
-        helperfunction(root);
-        return value==1;
+        solve(root);
+        return value;
     }
 };
