3276db17f2e64423757e90034822715f66bbeb11
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2023-10-30 13:28:44 +0530

    Time: 5 ms (21.39%) | Memory: 10.1 MB (30.37%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -11,67 +11,16 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root){
-        if(root==NULL){
-            return;
-        }
-        swap(root->left,root->right);
-        helper(root->left);
-        helper(root->right);
+void helper(TreeNode* root){
+    if(root==NULL){
+        return;
     }
+    swap(root->left,root->right);
+    helper(root->left);
+    helper(root->right);
+}
     TreeNode* invertTree(TreeNode* root) {
         helper(root);
         return root;
-        // if(root==NULL){
-        //     return NULL;
-        // }
-        // vector<int> v;
-        // queue<TreeNode*> q;
-        // q.push(root);
-        // v.push_back(root->val);
-        // while(!q.empty()){
-        //     TreeNode* node=q.front();
-        //     q.pop();
-        //     if(node->left){
-        //         q.push(node->left);
-        //         v.push_back(node->left->val);
-        //     }
-        //     else{
-        //         v.push_back(1000);
-        //     }
-        //     if(node->right){
-        //         q.push(node->right);
-        //         v.push_back(node->right->val);
-        //     }
-        //     else{
-        //         v.push_back(1000);
-        //     }
-        // }
-        // TreeNode* root1=new TreeNode(root->val);
-        // int i=1;
-        // q.push(root1);
-        // while(!q.empty()){
-        //     TreeNode* node=q.front();
-        //     q.pop();
-        //     if(v[i]==1000){
-        //         node->right=NULL;
-        //         i++;
-        //     }
-        //     else{
-        //         node->right=new TreeNode(v[i]);
-        //         q.push(node->right);
-        //         i++;
-        //     }
-        //     if(v[i]==1000){
-        //         node->left=NULL;
-        //         i++;
-        //     }
-        //     else{
-        //         node->left=new TreeNode(v[i]);
-        //         q.push(node->left);
-        //         i++;
-        //     }
-        // }
-        // return root1;
     }
 };
