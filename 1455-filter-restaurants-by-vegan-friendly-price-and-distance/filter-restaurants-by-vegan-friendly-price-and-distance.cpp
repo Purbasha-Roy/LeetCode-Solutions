64d71113f826c48a2ef9dc78e5a0d02879573af8
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2024-02-12 21:07:11 +0530

    Time: 67 ms (70.11%) | Memory: 37.1 MB (9.77%) - LeetSync

---
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> filterRestaurants(vector<vector<int>>& restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        vector<vector<int>> arr;
+        for(int i=0;i<restaurants.size();i++){
+            arr.push_back({restaurants[i][1],restaurants[i][0],restaurants[i][2],restaurants[i][3],restaurants[i][4]});
+        }
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        for(int i=0;i<arr.size();i++){
+            if(veganFriendly==1){
+                if(arr[i][2]==1 && arr[i][3]<=maxPrice && arr[i][4]<=maxDistance){
+                    ans.push_back(arr[i][1]);
+                }
+            }
+            else{
+                if(arr[i][3]<=maxPrice && arr[i][4]<=maxDistance){
+                    ans.push_back(arr[i][1]);
+                }
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
