56da311da4d2225c14eade57c3a062e2f95761e5
Purbasha-Roy <110253243+Purbasha-Roy@users.noreply.github.com>
2024-03-19 20:52:42 +0530

    Time: 11 ms (6.01%) | Memory: 15.2 MB (8.53%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
@@ -7,15 +7,22 @@ public:
             pq.push(amount[i]);
         }
         while(pq.top()!=0){
-            int a=pq.top();
-            pq.pop();
-            int b=pq.top();
-            pq.pop();
-            a--;
-            b--;
-            pq.push(a);
-            pq.push(b);
-            count++;
+            vector<int> temp;
+            for(int i=1;i<=2;i++){
+                if(!pq.empty()){
+                    temp.push_back(pq.top()-1);
+                    pq.pop();
+                }
+            }
+            for(int i=0;i<temp.size();i++){
+                pq.push(temp[i]);
+            }
+            if(temp.size()==2){
+                count++;
+            }
+            else{
+                count+=temp[0];
+            }
         }
         return count;
 
